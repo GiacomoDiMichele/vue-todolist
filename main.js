feb26ea60d616f2c5c73101647732a970606c74f
Giacomo <giacomo.1936@gmail.com>
2020-11-11 15:24:24 +0100

    prova function 1 aggiunta nuovo li

--- main.js
+++ main.js
@@ -2,12 +2,18 @@ var app = new Vue (
     {
         el:'#root',
         data: {
+            nuovo_todo: '',
             lista_todo: [
                 'telefonare marco',
                 'andare dal medico',
                 'studiare'
-            ]
-    }
+                ]
+            },
+        methods: {
+            aggiungiTodo(){
+                this.lista_todo.push(this.nuovo_todo)
+            }
+        }
 
     }
 )
