359ed5f7fae2ae726d6f9104a866f12a7df955b6
Giacomo <giacomo.1936@gmail.com>
2020-11-11 11:47:46 +0100

    creata lista to-do

---
+++ main.js
@@ -0,0 +1,13 @@
+var app = new Vue (
+    {
+        el:'#root',
+        data: {
+            lista_todo: [
+                'telefonare marco',
+                'andare dal medico',
+                'studiare'
+            ]
+    }
+
+    }
+)
